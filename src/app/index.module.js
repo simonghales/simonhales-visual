5c8249f1766c3803be102549246bd939158d680f
simonghales <simonghales@gmail.com>
2016-03-19 00:28:31 +1000

    basics

---
+++ src/app/index.module.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('simonhalesVisual', [
+      'ngAnimate',
+      'ui.router',
+      'toastr'
+    ]);
+
+})();
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('simonhalesVisual')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($log) {
+    var vm = this;
+  }
+
+})();
